0564ca6f172fd81081196f5303a853af6556ebf9
Maximilian  Stalgies <maximilian.stalgies@cimt-ag.de>
2023-09-05 09:25:33 +0200

    test

--- TippSpiel2023/main.py
+++ TippSpiel2023/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-url = 'https://github.com/LaDTo21/TippSpiel2023/blob/729f3e9b3f3fde423f4042f8a4507aec1e6eb0cf/OOF.csv?raw=true'
-df = pd.read_csv(url, index_col=0)
 
-print(df.info)
+url = "https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/rbsdm.comstats.csv"
+df = pd.read_csv(url)
+
+print(df['Player'])
