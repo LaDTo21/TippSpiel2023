5f1e8ff43badc5e00473386b59a0574c64938e19
Maximilian  Stalgies <maximilian.stalgies@cimt-ag.de>
2023-09-12 14:28:23 +0200

    Schedule daten anpassen

--- TippSpiel2023/data/testQuerries.py
+++ TippSpiel2023/data/testQuerries.py
@@ -1,8 +1,22 @@
+import datetime
+
+import pytz
+
 from TippSpiel2023.TippSpiel2023.data.statistics import Statistics
 
-for i in range(1, (Statistics.schedule_df.shape[1] - 1)):
-    print(Statistics.schedule_df.columns[i])
 
-print((Statistics.schedule_df.shape[1] - 1))
-print(Statistics.schedule_df.shape)
-print(Statistics.qb_df.shape)
+def change_column_dates_to_week_count():
+    timezone = pytz.timezone('Europe/Berlin')
+    for i in range(1, (Statistics.schedule_df.shape[1] - 1)):
+        print(Statistics.schedule_df.columns[i])
+        print(f'{datetime.datetime.now(tz=timezone).strftime("%Y")} '
+                                         f'{Statistics.schedule_df.columns[i]}')
+        print(datetime.datetime.strptime(f'{datetime.datetime.now(tz=timezone).strftime("%Y")} '
+                                         f'{Statistics.schedule_df.columns[i]}', "%Y %b %d").isocalendar()[1])
+
+    print((Statistics.schedule_df.shape[1] - 1))
+    print(Statistics.schedule_df.shape)
+    print(Statistics.qb_df.shape)
+
+
+change_column_dates_to_week_count()
--- TippSpiel2023/this_week.py
+++ TippSpiel2023/this_week.py
@@ -4,11 +4,11 @@ import pytz
 
 #eine Methode die den nächst größeren, bzw. kleineren Wert für local sucht
 timezone = pytz.timezone('Europe/Berlin')
-local = datetime.datetime.now(tz=timezone).strftime("%b. %w")
+local = datetime.datetime.now(tz=timezone).strftime("%b %w")
 week_count = datetime.datetime.now(tz=timezone).strftime("%U")
 
 url = "https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/2023_NFL_Schedule.csv"
 df = pd.read_csv(url)
 
 
-print()
+print(local)
