15a47e92efaa9b3e5368cd3d53e00b43845a79db
Maximilian  Stalgies <maximilian.stalgies@cimt-ag.de>
2023-11-14 17:40:46 +0100

    this just works better

--- TippSpiel2023/data/statistics.py
+++ TippSpiel2023/data/statistics.py
@@ -1,3 +1,5 @@
+import io
+
 import pandas as pd
 from dataclasses import dataclass
 import datetime
@@ -6,22 +8,42 @@ import requests
 from pandas import DataFrame
 
 
-class Statistics:
-    timezone = pytz.timezone('Europe/Berlin')
+class Renamer:
+    def __init__(self):
+        self.d = dict()
 
-    qb_df_url = "https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/rbsdm.comstats.csv"
-    schedule_df_url = "https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/2023_NFL_Schedule.csv"
-    defense_url = "C:\\Users\\mstalgies\\PycharmProjects\\TippSpiel_2023\\TippSpiel2023\\Defense.csv"
-    offense_url = "C:\\Users\\mstalgies\\PycharmProjects\\TippSpiel_2023\\TippSpiel2023\\Offense.csv"
-    defense_2022_url = "https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/Defense_2022.csv"
-    offense_2022_url = "https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/Offense_2022.csv"
-    qb_df = pd.read_csv(qb_df_url)
+    def __call__(self, x):
+        if x not in self.d:
+            self.d[x] = 0
+            return x
+        else:
+            self.d[x] += 1
+            return "%s_%d" % (x, self.d[x])
+
+
+class Statistics:
+    @staticmethod
+    def prepare_tables(url, table_count):
+        df = (pd.read_html(io.StringIO(requests.get(url).text
+                                       .replace("<!--", "")
+                                       .replace("-->", "")))[table_count]
+              .iloc[:-3]
+              .rename(columns=Renamer()))
+        df.columns = df.columns.droplevel(0)
+        return df
+
+    schedule_df_url = 'https://raw.githubusercontent.com/LaDTo21/TippSpiel2023/main/2023_NFL_Schedule.csv'
+    defense_url = 'https://www.pro-football-reference.com/years/2023/opp.htm#team_stats'
+    offense_url = 'https://www.pro-football-reference.com/years/2023/#team_stats'
+    defense_2022_url = 'https://www.pro-football-reference.com/years/2022/opp.htm#team_stats'
+    offense_2022_url = 'https://www.pro-football-reference.com/years/2022/#all_team_stats'
     schedule_df = pd.read_csv(schedule_df_url, sep=';')
-    defense_df = pd.read_csv(defense_url)
-    offense_df = pd.read_csv(offense_url)
-    defense_2022_df = pd.read_csv(defense_2022_url)
-    offense_2022_df = pd.read_csv(offense_2022_url)
-    defense_2022_ranks_df = defense_2022_df[['Tm', 'G', 'PA']].copy()
+    defense_df = prepare_tables(defense_url, 0)
+    offense_df = prepare_tables(offense_url, 4)
+    defense_2022_df = prepare_tables(defense_2022_url, 0)
+    offense_2022_df = prepare_tables(offense_2022_url, 5)
+
+    timezone = pytz.timezone('Europe/Berlin')
 
     for i in range(1, (schedule_df.shape[1])):
         schedule_df.rename(columns={schedule_df.columns[i]: datetime.datetime.strptime(
--- TippSpiel2023/this_week.py
+++ TippSpiel2023/this_week.py
@@ -5,38 +5,58 @@ from TippSpiel2023.TippSpiel2023.data.teams import Teams, Team
 from decimal import Decimal, ROUND_HALF_UP
 
 
-def get_winner(team1: Team, team2: Team):
-    score1 = score2 = 0
-    score1 += Decimal(team1.offense_exp * 100 / Statistics.offense_df[team1.list[0]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal(team2.offense_exp * 100 / Statistics.offense_df[team2.list[0]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score1 += Decimal(team1.offense_yp * 100 / Statistics.offense_df[team1.list[1]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal(team2.offense_yp * 100 / Statistics.offense_df[team2.list[1]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score1 += Decimal(team1.offense_1std * 100 / Statistics.offense_df[team1.list[2]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal(team2.offense_1std * 100 / Statistics.offense_df[team2.list[2]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score1 += Decimal(team1.offense_pass_td * 100 / Statistics.offense_df[team1.list[3]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal(team2.offense_pass_td * 100 / Statistics.offense_df[team2.list[3]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score1 += Decimal(team1.defense_pa * 100 / Statistics.offense_df[team1.list[4]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal(team2.defense_pa * 100 / Statistics.offense_df[team2.list[4]].max()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score1 += Decimal((Statistics.defense_df[team1.list[5]].min() - team1.defense_yp) * 100 / Statistics.defense_df[team1.list[5]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal((Statistics.defense_df[team2.list[5]].min() - team2.defense_yp) * 100 / Statistics.defense_df[team2.list[5]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score1 += Decimal((Statistics.defense_df[team1.list[6]].min() - team1.defense_1std) * 100 / Statistics.defense_df[team1.list[6]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal((Statistics.defense_df[team2.list[6]].min() - team2.defense_1std) * 100 / Statistics.defense_df[team2.list[6]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    print(score1, score2)
-    score1 += Decimal((Statistics.defense_df[team1.list[7]].min() - team1.defense_rush_td) * 100 / Statistics.defense_df[team1.list[7]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    score2 += Decimal((Statistics.defense_df[team2.list[7]].min() - team2.defense_rush_td) * 100 / Statistics.defense_df[team2.list[7]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
-    print(score1, score2)
-    winner = team1.name if score1 > score2 else team2.name
-    return f'{team1.name} vs. {team2.name} : {winner}'
-
-
 class ThisWeek:
+    @staticmethod
+    def get_winner(team1: Team, team2: Team):
+        score1 = score2 = 0
+        score1 += Decimal(team1.offense_exp * 100 / Statistics.offense_df[team1.list[0]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(team2.offense_exp * 100 / Statistics.offense_df[team2.list[0]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score1 += Decimal(team1.offense_yp * 100 / Statistics.offense_df[team1.list[1]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(team2.offense_yp * 100 / Statistics.offense_df[team2.list[1]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score1 += Decimal(team1.offense_1std * 100 / Statistics.offense_df[team1.list[2]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(team2.offense_1std * 100 / Statistics.offense_df[team2.list[2]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score1 += Decimal(team1.offense_pass_td * 100 / Statistics.offense_df[team1.list[3]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(team2.offense_pass_td * 100 / Statistics.offense_df[team2.list[3]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score1 += Decimal(team1.defense_exp * 100 / Statistics.offense_df[team1.list[4]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(team2.defense_exp * 100 / Statistics.offense_df[team2.list[4]].max()).quantize(
+            Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score1 += Decimal((Statistics.defense_df[team1.list[5]].min() - team1.defense_yp) * 100 / Statistics.defense_df[
+            team1.list[5]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal((Statistics.defense_df[team2.list[5]].min() - team2.defense_yp) * 100 / Statistics.defense_df[
+            team2.list[5]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score1 += Decimal(
+            (Statistics.defense_df[team1.list[6]].min() - team1.defense_1std) * 100 / Statistics.defense_df[
+                team1.list[6]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(
+            (Statistics.defense_df[team2.list[6]].min() - team2.defense_1std) * 100 / Statistics.defense_df[
+                team2.list[6]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        print(score1, score2)
+        score1 += Decimal(
+            (Statistics.defense_df[team1.list[7]].min() - team1.defense_rush_td) * 100 / Statistics.defense_df[
+                team1.list[7]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        score2 += Decimal(
+            (Statistics.defense_df[team2.list[7]].min() - team2.defense_rush_td) * 100 / Statistics.defense_df[
+                team2.list[7]].min()).quantize(Decimal('1.1111'), rounding=ROUND_HALF_UP)
+        print(score1, score2)
+        winner = team1.name if score1 > score2 else team2.name
+        return f'{team1.name} vs. {team2.name} : {winner}'
+
     temp_team_list = Teams.all.copy()
     timezone = pytz.timezone('Europe/Berlin')
     week_count = int(datetime.datetime.now(tz=timezone).strftime("%U"))
     count = 0
     for team in Teams.all:
         team2 = 0
-        temp_string = Statistics.schedule_df[45].loc[Statistics.schedule_df['Teams'] == Teams.all[team].name][count]
+        temp_string = Statistics.schedule_df[week_count].loc[Statistics.schedule_df['Teams'] == Teams.all[team].name][count]
         if temp_string.startswith(('@', 'vs.')):
             temp_string = temp_string.split()[1]
             for team3 in Teams.all:
